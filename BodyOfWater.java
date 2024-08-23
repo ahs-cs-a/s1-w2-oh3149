9804169ed9331288bd6016b947ea7e01e2e9e9c9
Andrew <38505680+oh3149@users.noreply.github.com>
2024-08-23 19:40:16 +0000

    bfgf
    a

--- BodyOfWater.java
+++ BodyOfWater.java
@@ -1,1 +1,54 @@
+public class BodyOfWater {
+    private String name;
+    private int largestDiameter;
+    private int avgDepth;
+    private boolean isSaltWater;
+
+    public BodyOfWater(String n, int l, int d, boolean s){
+        name = n;
+        largestDiameter = l;
+        avgDepth = d;
+        isSaltWater = s;
+    }
+
+    public BodyOfWater(String n, int d){
+        name = n;
+        avgDepth = d;
+    }
+
+    public void setName(String n){
+        name = n;
+    }
+
+    public void setLargestDiameter(int s){
+        largestDiameter = s;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getLargestDiameter() {
+        return largestDiameter;
+    }
+
+    public int getAvgDepth() {
+        return avgDepth;
+    }
+
+    public boolean isSaltWater() {
+        return isSaltWater;
+    }
+
+    public void printState(){
+        System.out.println("Name: " + name);
+        System.out.println("Largest Diameter: " + largestDiameter);
+        System.out.println("Average Depth: " + avgDepth);
+        System.out.println("Is Salt Water: " + isSaltWater);
+
+    }
+
+
+}
+
 
--- BodyOfWaterMain.java
+++ BodyOfWaterMain.java
@@ -1,6 +1,29 @@
 public class BodyOfWaterMain {
     public static void main(String[] args) {
-        BodyOfWater b = new BodyOfWater();
+        BodyOfWater b = new BodyOfWater("Lake", 100, 50, false);
+        BodyOfWater b1 = new BodyOfWater("Ocean", 1000, 10000, true);
+        BodyOfWater b2 = new BodyOfWater("Pond", 10);
         System.out.println("here");
+
+        System.out.println(b.getName());
+        System.out.println(b.getLargestDiameter());
+        System.out.println(b.getAvgDepth());
+        System.out.println(b.isSaltWater());
+
+        System.out.println(b1.getName());
+        System.out.println(b1.getLargestDiameter());
+        System.out.println(b1.getAvgDepth());
+        System.out.println(b1.isSaltWater());
+
+        System.out.println(b2.getName());
+        System.out.println(b2.getAvgDepth());
+
+
+        b.setName("Lake1");
+        
+        System.out.println(b.getName());
+
+        b1.printState();
+
     }
 }
